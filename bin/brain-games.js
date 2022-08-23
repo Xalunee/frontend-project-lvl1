26c3c36b46b4db7d503a5216534082ec5f045a22
temasemyonov678gh <temasemyonov678@gmail.com>
2022-08-23 19:40:24 +0500

    add cli.js, .gitignore, package-lock.json and removed brain-games.js, package.json

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,2 +1,5 @@
 #!/usr/bin/env node
-console.log('node bin/brain-games.js\nWelcome to the Brain Games!')
+import { greetingWithUser } from './cli.js';
+
+console.log('node bin/brain-games.js\nWelcome to the Brain Games!');
+greetingWithUser();
