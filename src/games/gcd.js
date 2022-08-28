557069de2892c8c9757f75d39a1ade47fb226d66
temasemyonov678gh <temasemyonov678@gmail.com>
2022-08-28 14:53:55 +0500

    fix gcd.js

--- src/games/gcd.js
+++ src/games/gcd.js
@@ -1,18 +1,27 @@
 import getGame from '../index.js';
 
+const randomInteger = () => {
+  let int = Math.round(Math.random() * 100);
+  while (int < 1) {
+    int = Math.round(Math.random() * 100);
+  }
+
+  return int;
+};
+
 const gameGcd = () => {
   const question = 'Find the greatest common divisor of given numbers.';
   const expressions = [];
   const results = [];
 
   for (let i = 0; i < 3; i += 1) {
-    const firstNumber = Math.round(Math.random() * 100);
-    const secondNumber = Math.round(Math.random() * 100);
+    const firstNumber = randomInteger();
+    const secondNumber = randomInteger();
     const max = firstNumber > secondNumber ? firstNumber : secondNumber;
     const min = firstNumber < secondNumber ? firstNumber : secondNumber;
     let gcd;
 
-    for (let num = 0; num <= min; num += 1) {
+    for (let num = 1; num <= min; num += 1) {
       if (max % num === 0 && min % num === 0) {
         gcd = num;
       }
